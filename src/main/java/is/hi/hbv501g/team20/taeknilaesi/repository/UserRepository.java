27ccf5edcc22d83f8d5750243c2df4985aa838eb
sthb13 <42750168+sthb13@users.noreply.github.com>
2020-10-13 20:24:00 +0000

    Add files via upload

---
+++ src/main/java/is/hi/hbv501g/team20/taeknilaesi/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package is.hi.hbv501g.team20.taeknilaesi.repository;
+
+import is.hi.hbv501g.team20.taeknilaesi.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>
+{
+}
