eb7ed7feb514cfab21664caa31d50869203e8ff3
fas19 <47185536+fas19@users.noreply.github.com>
2020-11-03 18:23:38 +0000

    Quiz fyrir kúrs 1 & 3. Vistast svo í progress

--- src/main/java/is/hi/hbv501g/team20/taeknilaesi/controller/QuizController.java
+++ src/main/java/is/hi/hbv501g/team20/taeknilaesi/controller/QuizController.java
@@ -1,12 +1,7 @@
 package is.hi.hbv501g.team20.taeknilaesi.controller;
 
-import is.hi.hbv501g.team20.taeknilaesi.model.Lesson;
-import is.hi.hbv501g.team20.taeknilaesi.model.Progress;
-import is.hi.hbv501g.team20.taeknilaesi.model.Question;
-import is.hi.hbv501g.team20.taeknilaesi.model.User;
-import is.hi.hbv501g.team20.taeknilaesi.service.ProgressService;
-import is.hi.hbv501g.team20.taeknilaesi.service.QuestionService;
-import is.hi.hbv501g.team20.taeknilaesi.service.QuizService;
+import is.hi.hbv501g.team20.taeknilaesi.model.*;
+import is.hi.hbv501g.team20.taeknilaesi.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
@@ -30,13 +25,42 @@ public class QuizController {
     @Autowired
     ProgressService progressService;
 
+    @Autowired
+    CourseService courseService;
+
+    @Autowired
+    UserService userService;
+
 
     @GetMapping("/quiz/{courseId}")
-    private String finishLesson(@PathVariable("courseId") int courseId,  @CurrentSecurityContext(expression = "authentication.name") String username, HttpSession session, Model model) throws Exception {
+    private String takeQuiz(@PathVariable("courseId") int courseId,  @CurrentSecurityContext(expression = "authentication.name") String username, HttpSession session, Model model) throws Exception {
         List<Question> q = questionService.getQuestions(courseId);
 
+        model.addAttribute("courseId", courseId);
         model.addAttribute("questions",q);
         return "quiz";
+    }
+
+    @GetMapping("/quiz/{courseId}/finish")
+    private ModelAndView finishQuiz(@PathVariable("courseId") int courseId,  @CurrentSecurityContext(expression = "authentication.name") String username, HttpSession session, Model model) throws Exception {
+
+        // Ehv reikingar til að finna einkunn og ehv fleira
+        //Mögulega tala við progressService til að bera saman einkunn ef a að taka quiz oft til að fa betri einkunn
+
+        // Þetta er bara til að setja i progress, þarf að utfæra betur!
+
+        User user = userService.findUserByUsername(username);
+        Quiz q = quizService.findQuizByCourseId(courseId);
+        if(user!=null && q !=null) {
+            Progress p = new Progress(q, user);
+            progressService.save(p);
+            System.out.println(user.getName()+" just finished quiz in course "+q.getCourse().getDescription());
+        }
+
+
+        model.addAttribute("courseId", courseId);
+        model.addAttribute("questions",q);
+        return new ModelAndView("redirect:/");
     }
 
 
--- src/main/java/is/hi/hbv501g/team20/taeknilaesi/service/QuizService.java
+++ src/main/java/is/hi/hbv501g/team20/taeknilaesi/service/QuizService.java
@@ -1,6 +1,7 @@
 package is.hi.hbv501g.team20.taeknilaesi.service;
 
 import is.hi.hbv501g.team20.taeknilaesi.model.Answer;
+import is.hi.hbv501g.team20.taeknilaesi.model.Quiz;
 import is.hi.hbv501g.team20.taeknilaesi.repository.QuizRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,20 @@ public class QuizService {
     @Autowired
     QuizRepo qs;
     // fá slembið quiz
+
+
+    public Quiz findQuizByCourseId(int courseId){
+        Iterable<Quiz> all = qs.findAll();
+
+        for(Quiz q : all){
+            if(q.getCourse()!=null){
+                if(q.getCourse().getId()==courseId){
+                    return q;
+                }
+            }
+        }
+        return null;
+
+    }
 
 }
